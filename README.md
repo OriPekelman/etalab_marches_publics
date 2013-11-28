21cfad767f312eb48c495636fd8411eab876e2c0
Administrator <ori@pekelman.com>
2013-11-28 16:24:43 +0100

    beaucoup trop fatigué pour arriver à faire quoi que ce soit. Je commite pour référence future.

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ etalab_marches_publics
 
 Etude de données marchés publics
 2010 la données est trop pourrie pour être même inspectée.
+
+sed 's/\"/“/g'  initial_data/siren.csv > initial_data/siren_quoted.csv
+curl http://localhost:9200/etalab_siren_2012/_search -d'
+{
+  "query" : {
+    "fuzzy_like_this" : {
+       "fields" : ["postal_code", "commerce_tribunal"],
+       "like_text" : "ID ACT DEVELOPPEMENT",
+       "max_query_terms" : 12
+    }
+  }
+}'
---
+++ scripts/clean/etalab.rb
@@ -0,0 +1,43 @@
+require "simple-spreadsheet"
+require "json"
+require 'elasticsearch'
+require "pry"
+
+class Etalab
+end
+
+class Etalab::Importer
+
+  def initialize(file_name, mapping, index_name)
+    @mapping = mapping
+    @index_name = "etalab_#{index_name}"
+    @h = {}
+
+    # Connect to localhost:9200 by default:
+    @es = Elasticsearch::Client.new log: true
+  end
+  
+  def index!(doc,id)
+    # Index a document:
+    @es.index index: @index_name,
+             type:  @index_name,
+             id: id,
+             body: doc
+  end
+  
+  
+  def import!
+
+    @first_row.upto(@last_row) do |line|
+
+      @mapping.each_with_index do |field, column|
+        key = field.keys[0]
+        val =  self.public_send(key, @s.cell(line, column +1))  
+        puts "." if (column % 10)==0
+        @h[key] = val
+      end
+      self.index!(@h, line)
+    end
+    @last_row
+  end
+end
